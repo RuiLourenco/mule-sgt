ec730b44f6cedc5d3e5b4ed2db5cdefcc2b812b5
RuiLourenco <rui.lourenco@smt.ufrj.br>
2024-06-20 16:51:05 +0000

    Using at::Tensor instead of a circular buffer.

--- src/LightField/include/LightField/LightField.h
+++ src/LightField/include/LightField/LightField.h
@@ -1,4 +1,4 @@
-#include "View.h"
+#include <torch/torch.h>
 
 #ifndef LIGHTFIELD_H
 #define LIGHTFIELD_H
@@ -9,9 +9,9 @@ public:
     char *mViewFileNameSuffix;          /*!< lightfield view name: <mViewFileNamePrefix>_<horizontal index>_<vertical index>_<mViewFilenameSuffix>.pgm */
     int mNumberOfHorizontalDigits;      /*!< number of digits used to represent the horizontal index in the view file names */
     int mNumberOfVerticalDigits;        /*!< number of digits used to represent the vertical index in the view file names */
-     int mNumberOfHorizontalViews;      /*!< total number of horizontal views of the lightfield */
+    int mNumberOfHorizontalViews;      /*!< total number of horizontal views of the lightfield */
     int mNumberOfVerticalViews;         /*!< total number of vertical views of the lightfield */
-    View **mViewCache;                  /*!< pointer to the two dimensional circular separable cache of views*/
+    at::Tensor data;                  /*!< pointer to the two dimensional circular separable cache of views*/
     int mNumberOfCacheHorizontalViews;  /*!< number of horizontal views of the lightfield in the cache */
     int mNumberOfCacheVerticalViews;    /*!< number of vertical views of the lightfield in the cache */
     int mFirstCacheHorizontalView;      /*!< horizontal index of the first cached view */
@@ -34,6 +34,8 @@ public:
     void WriteBlock4DtoLightField(Block4D *targetBlock, int position_t, int position_s, int position_v, int position_u, int component=0);
     int FindViewFileName(char *viewFileName, int index_t, int index_s);
     void SetViewVerbosity(char verbosity);
+    
+    
 };
 
 #endif
