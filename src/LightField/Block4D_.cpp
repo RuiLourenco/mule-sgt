57f1648490196e38a5d6f85fbd194132dead0bc8
RuiLourenco <rui.lourenco@smt.ufrj.br>
2024-06-25 12:26:01 +0000

    Implemented functions mirroring Block.h

--- src/LightField/Block4D_.cpp
+++ src/LightField/Block4D_.cpp
@@ -1,8 +1,123 @@
-#include "Block4D_.h"
+#include "LightField/Block4D_.h"
 
-Block4D_::Block4D(std::array<int,4> size) {
-    data = torch::zeros({size[0], size[1], size[2], size[3]});
+
+Block4D_::operator at::Tensor() const{
+    return this->data;
 }
-Block4D_::Block4D(const Block4D_& B00, const Block4D_& B01, const Block4D_& B10, const Block4D_& B11) {
-    data = torch::cat({torch::cat({B00.data, B01.data}, 0),torch::cat({B10.data, B11.data}, 0)},1);
+
+Block4D_::Block4D_(const at::Tensor& data){
+    this->data = data;
+}   
+Block4D_::Block4D_(std::array<int,4> size) {
+    this->data = torch::zeros({size[0], size[1], size[2], size[3]}, torch::kShort);
 }
+
+/**
+ * Constructs a Block4D_ object by concatenating the data from four equal sized Block4D_ objects in a specific pattern.
+ *
+ * @param B00 The first Block4D_ object.
+ * @param B01 The second Block4D_ object.
+ * @param B10 The third Block4D_ object.
+ * @param B11 The fourth Block4D_ object.
+ *
+ * @return None
+ *
+ * @throws None
+ */
+Block4D_::Block4D_(const Block4D_& B00, const Block4D_& B01, const Block4D_& B10, const Block4D_& B11,bool views) {
+    int x1,x2;
+    if(views){
+        x1 = 0; x2 = 1;
+    }else{
+        x1 = 2; x2 = 3;
+    }
+    assert(B00.data.sizes() == B01.data.sizes() && B01.data.sizes() == B10.data.sizes() && B10.data.sizes() == B11.data.sizes() && "All Block4D_ objects must have the same shape.");
+    this->data = torch::cat({torch::cat({B00.data, B01.data}, x1),torch::cat({B10.data, B11.data}, x1)},x2);
+}
+/**
+ * Copies a subblock from another Block4D_ object to the current Block4D_ object.
+ *
+ * @param B The Block4D_ object from which the subblock is copied.
+ * @param sourceOffset An array of 4 integers representing the starting index of the source subblock.
+ * @param targetOffset An array of 4 integers representing the starting index of the target subblock.
+ *
+ * @throws None
+ */
+void Block4D_::CopySubblockFrom(const Block4D_ &B, std::array<int,4> sourceOffset, std::array<int,4> targetOffset){
+    this->data.index({at::indexing::Slice(targetOffset[0],this->data.size(0)-1),
+                          at::indexing::Slice(targetOffset[1],this->data.size(1)-1),
+                          at::indexing::Slice(targetOffset[2],this->data.size(2)-1),
+                          at::indexing::Slice(targetOffset[3],this->data.size(3)-1)}) =
+                          B.data.index({at::indexing::Slice(sourceOffset[0],this->data.size(0)-1),
+                                        at::indexing::Slice(sourceOffset[1],this->data.size(1)-1),
+                                        at::indexing::Slice(sourceOffset[2],this->data.size(2)-1),
+                                        at::indexing::Slice(sourceOffset[3],this->data.size(3)-1)
+                                        });
+}
+
+
+void Block4D_::Ones(){
+    this->data = torch::ones(this->data.sizes(),this->data.dtype());
+}
+void Block4D_::Zeros(){
+    this->data = torch::zeros(this->data.sizes(),this->data.dtype());
+}
+void Block4D_::Shift_UVPlane(int shift, int position_t, int position_s){
+   //std::cout<<this->data[position_t][position_s]<<std::endl<<std::endl;
+    if(shift > 0){
+        this->data[position_t][position_s] = this->data[position_t][position_s].bitwise_left_shift(shift);
+    }
+    if(shift < 0){
+        this->data[position_t][position_s] = this->data[position_t][position_s].bitwise_right_shift(-shift);
+    }
+    //std::cout<<this->data[position_t][position_s]<<std::endl<<std::endl;
+}
+
+Block4D_ Block4D_::operator + (const Block4D_ &B){
+    return Block4D_(this->data + B.data);
+}
+Block4D_ Block4D_::operator * (const Block4D_ &B){
+    return Block4D_(this->data * B.data);
+}
+Block4D_ Block4D_::operator - (const Block4D_ &B){
+    return Block4D_(this->data - B.data);
+}
+Block4D_ Block4D_::operator / (const int &a){
+    return Block4D_(this->data / a);
+}
+Block4D_ Block4D_::operator + (const int &a){
+    return Block4D_(this->data + a);
+}
+Block4D_ Block4D_::operator - (const int &a){
+    return Block4D_(this->data - a);
+}
+Block4D_ Block4D_::operator * (const int &a){
+    return Block4D_(this->data * a);
+}
+void Block4D_::operator += (const Block4D_ &B){
+    this->data = this->data + B.data;
+}
+void Block4D_::operator -= (const Block4D_ &B){
+    this->data = this->data - B.data;
+}
+void Block4D_::operator *= (const Block4D_ &B){
+    this->data = this->data * B.data;
+}
+void Block4D_::operator = (const Block4D_ &B){
+    this->data = B.data;
+}
+
+Block4D_ Block4D_::clone(){
+    return this->data.clone();
+}
+
+
+
+// void Block4D_::Shift_UVPlane(int shift, int position_t, int position_s) {
+
+//     this->data[position_t][position_s] = this->data[position_t][position_s]<<shift;
+
+// }
+
+
+
--- src/LightField/include/LightField/Block4D_.h
+++ src/LightField/include/LightField/Block4D_.h
@@ -13,16 +13,37 @@ class Block4D_
 public: 
     at::Tensor data;
     operator at::Tensor() const;
+    //operator const at::Tensor&() const;
     Block4D_() = default;
+    //Block4D_(at::Tensor data);
+    Block4D_(const at::Tensor& data);
+    //Block4D_(at::Tensor& data);
     Block4D_(std::array<int,4> size);
-    Block4D_(const Block4D_& B00, const Block4D_& B01, const Block4D_& B10, const Block4D_& B11);
+    Block4D_(const Block4D_& B00, const Block4D_& B01, const Block4D_& B10, const Block4D_& B11, bool views);
+    void Shift_UVPlane(int shift, int position_t, int position_s);
+    void Ones(void);
+    void Zeros(void);
+    
+
+    Block4D_ operator + (const Block4D_ &B);
+    Block4D_ operator * (const Block4D_ &B);
+    Block4D_ operator - (const Block4D_ &B);
+    Block4D_ operator + (const int &a);
+    Block4D_ operator * (const int &a);
+    Block4D_ operator - (const int &a);
+    Block4D_ operator / (const int &a);
+    void operator += (const Block4D_ &B);
+    void operator *= (const Block4D_ &B);
+    void operator -= (const Block4D_ &B);
+    void operator = (const Block4D_ &B);
+    void operator = (Block4D_* B);
+    Block4D_ clone();
+
     ~Block4D_() = default;
-    void fromBlock(const Block4D_ &B, int source_offset_t, int source_offset_s, int source_offset_v, int source_offset_u, int target_offset_t=0, int target_offset_s=0, int target_offset_v=0, int target_offset_u=0););
     
     void CopySubblockFrom(const Block4D_ &B, std::array<int,4> sourceOffset, std::array<int,4> targetOffset); 
 
-    void Ones(void);
-    void Zeros(void);
+
     void Display(void);
     void DCT_U(int scale);
     void IDCT_U(int scale);
@@ -45,19 +66,18 @@ public:
     void Extend_T(int position_t);
     void Clip(int minValue, int maxValue);
     void Threshold(int minMagnitude, int maxMagnitude);
-    void Shift_UVPlane(int shift, int position_t, int position_s);
 
-	block4DElementType GetPixel(int position_t, int position_s, int position_v, int position_u) {
-		return(mPixelData[LinearPosition(position_t, position_s, position_v, position_u)]);
+	short GetPixel(int position_t, int position_s, int position_v, int position_u) {
+		return(data[position_t][position_s][position_v][position_u].item<short>());
 	}
 
 	void SetPixel(block4DElementType pixel_value, int position_t, int position_s, int position_v, int position_u) {
-		mPixelData[LinearPosition(position_t, position_s, position_v, position_u)] = pixel_value;
-	}
+		data[position_t][position_s][position_v][position_u] = pixel_value;
+    }
 
 	long int LinearPosition(long int position_t, long int position_s, long int position_v, long int position_u) {
-		long int linear_position = position_t*mlength_u*mlength_v*mlength_s;
-		linear_position += position_s*mlength_u*mlength_v + position_v*mlength_u + position_u;
+		long int linear_position = position_t*data.size(3)*data.size(2)*data.size(1);
+		linear_position += position_s*data.size(3)*data.size(2) + position_v*data.size(3) + position_u;
 		return(linear_position);
 	}
 
