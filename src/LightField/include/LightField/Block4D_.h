4cade1c734f08db7fcee5f78a265f923e4cd151c
RuiLourenco <rui.lourenco@smt.ufrj.br>
2025-02-27 13:42:54 +0000

    Constant Change

--- src/LightField/include/LightField/Block4D_.h
+++ src/LightField/include/LightField/Block4D_.h
@@ -40,7 +40,7 @@ class SgtSideInfo{
         static constexpr double PRECISION_RHO = 1e-5; 
         static constexpr double MIN_RHO = 0.2;
         static constexpr double MAX_RHO = 1-1e-5;
-        static constexpr double PRECISION_ANGLE = 10;  
+        static constexpr double PRECISION_ANGLE = 3;  
         void print();
         SgtSideInfo(const Block4D_& block,std::array<double,2> dispRange);
         SgtSideInfo(int RhoSInt,int RhoTInt,int RhoUInt,int RhoVInt,int angleVInt,int angleHInt, std::array<double,2> dispRange);
