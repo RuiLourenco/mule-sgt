c55d84695f4da5ce084dae470a2f52b6c3978a81
RuiLourenco <rui.lourenco@smt.ufrj.br>
2025-07-17 18:30:24 +0000

    uncommented computeGradients()

--- src/LightField/LightField.cpp
+++ src/LightField/LightField.cpp
@@ -397,7 +397,7 @@ void LightField :: OpenLightFieldPPM_(std::string rootPath, std::string pattern,
         this->data = io::read_collection(rootPath, pattern,this->mPGMScale).to(torch::kInt16);
         std::cout<<"First View: "<<firstView[0]<<" "<<firstView[1]<<" View Size: "<<viewSize[0]<<" "<<viewSize[1]<<std::endl;
         this->data = this->data.index({at::indexing::Slice({firstView[0],firstView[0]+viewSize[0]}),at::indexing::Slice({firstView[1],firstView[1]+viewSize[1]}),at::indexing::Slice(),at::indexing::Slice()});
-        //computeGradients();
+        computeGradients();
 }
 void LightField :: OpenLightFieldPPM_(std::string rootPath, std::string pattern, char readOrWriteLightField,std::array<int64_t,2> firstView = {0,0} ) {
     if(readOrWriteLightField == 'r'){
