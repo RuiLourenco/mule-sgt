9de16bdc1023f210193f6218f225dd014999d4e9
RuiLourenco <rui.lourenco@smt.ufrj.br>
2025-07-17 18:30:52 +0000

    added default values to setSpatialRhos and setAngularRhos

--- src/LightField/include/LightField/Block4D_.h
+++ src/LightField/include/LightField/Block4D_.h
@@ -114,7 +114,7 @@ class SgtSideInfo{
         void estimateRhosLS(const at::Tensor& covFun, bool isHorizontal);
         static double rhoFromCov(const at::Tensor& cov);
         void setSpatialRhos(const at::Tensor& covFunH, const at::Tensor& covFunV);
-        void setSpatialRhos(const double rhoU, const double rhoV);
+        void setSpatialRhos(const double rhoU = FIXED_SPATIAL_RHO, const double rhoV = FIXED_SPATIAL_RHO);
         void setAngularRhos(const double rhoS = FIXED_ANGULAR_RHO, const double rhoT = FIXED_ANGULAR_RHO);
         
 
